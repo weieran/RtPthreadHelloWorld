6803b544787be055c17285fbf5bca7e57fd773b4
Weier Andreas <andreas.weier@citycable.ch>
2016-05-01 20:53:35 +0200

    add conditional variable

--- main.cpp
+++ main.cpp
@@ -13,48 +13,94 @@
 #include <unistd.h>
 #include <array>
 
-const size_t NBR_OF_THREADS = 5;
-
-
-void* mainThread1(void *threadId)
+namespace
 {
-	__useconds_t sleeepTime_us = 1 * 1000000;//1sek
+
+	const size_t MAX_NBR_OF_THREADS = 5;
+
+	struct ThreadData
+	{
+		size_t threadId;
+		size_t prio;
+		__useconds_t sleepTime_us;
+		std::string message;
+	};
+
+	pthread_mutex_t countMutex;
+	pthread_cond_t countThreshold_cv;
+	const auto COUNT_LIMIT = 10;
+	size_t count;
+
+	void countAndSignal ()
+	{
+	  pthread_mutex_lock (&countMutex);
+	  count++;
+	  if (count >= COUNT_LIMIT)
+	  {
+	    pthread_cond_signal (&countThreshold_cv);
+	  }
+	  pthread_mutex_unlock (&countMutex);
+	}
+}
+
+
+
+void* mainThread1(void *threadArg)
+{
+	auto data = static_cast<ThreadData*>(threadArg);
 
   while(1)
   {
-	  printf("Thread PID: #%d!is alive\n", (uint32_t)threadId);
-	  usleep(sleeepTime_us);
+	  pthread_mutex_lock (&countMutex);
+	  pthread_cond_wait(&countThreshold_cv, &countMutex);
+	  pthread_mutex_unlock (&countMutex);
+
+	  printf("Thread PID: #%d!is alive\n", data->threadId);
+
+	  countAndSignal();
   }
 
 };
 
-void* mainThread2(void *threadId)
+void* mainThread2(void *threadArg)
 {
-	__useconds_t sleeepTime_us = 2 * 1000000;// 2sek
+	auto data = static_cast<ThreadData*>(threadArg);
 
   while(1)
   {
-	  printf("Thread PID: #%d! is alive\n", (uint32_t)threadId);
-	  usleep(sleeepTime_us);
+	  printf("Thread PID: #%d!is alive\n", data->threadId);
+	  usleep(data->sleepTime_us);
   }
 
 };
-
-
 
 
 int main(int argc,char** argv)
 {
-   std::array<pthread_t, NBR_OF_THREADS> threads;
+   std::array<pthread_t, MAX_NBR_OF_THREADS> threads;
+
+   const ThreadData data = { 1, 50, 1000000, "first thread"};
+   std::array<ThreadData, MAX_NBR_OF_THREADS> threadData = {{
+		   //Id,Prio, Sleep_us, MessageString
+                   { 1, 50, 1000000, "first thread"},
+		   { 2, 50, 2000000,"second thread"},
+		   { 3, 50, 3000000,"third thread"},
+		   { 4, 50, 4000000,"fourth thread"},
+		   { 5, 50, 5000000,"fifth thread"}
+   }};
 
 
-   pthread_create(&threads[0], nullptr, mainThread1, (void*)0);
-   pthread_create(&threads[1], nullptr, mainThread1, (void*)1);
+   pthread_mutex_init(&countMutex, nullptr);
+   pthread_cond_init(&countThreshold_cv, nullptr);
+
+   pthread_create(&threads[0], nullptr, mainThread1, &threadData[0]);
+   pthread_create(&threads[1], nullptr, mainThread1, &threadData[1]);
 
 
    while(1){
 
 	  printf("Main Thread alive \n");
+	  countAndSignal();
 	  sleep(3);//3 Sek
 
    }
